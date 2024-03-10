9d97e6f12d63d266e1468afd448e1e0c0d95b571
arimaxx <155280132+arimaxx@users.noreply.github.com>
2024-03-10 22:23:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
 #ARIOP
+import re
+from os import getenv
+from dotenv import load_dotenv
+from pyrogram import filters
+
+load_dotenv()
+
+
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://ariopp:ariopp@cluster0.fpdbynt.mongodb.net/?retryWrites=true&w=majority")
