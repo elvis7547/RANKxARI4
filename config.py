c998fa454b32d13432bfcd88a17911371023bd55
elvis7547 <161116549+elvis7547@users.noreply.github.com>
2024-03-11 22:09:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,5 @@ from os import getenv
 from dotenv import load_dotenv
 from pyrogram import filters
 
-load_dotenv()
-
 
 MONGO_DB_URI = "mongodb+srv://ariopp:ariopp@cluster0.fpdbynt.mongodb.net/?retryWrites=true&w=majority"
