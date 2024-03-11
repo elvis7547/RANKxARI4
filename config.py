2daf2a129888843d455820a936ad149d0795df02
elvis7547 <161116549+elvis7547@users.noreply.github.com>
2024-03-11 22:07:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ from pyrogram import filters
 load_dotenv()
 
 
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://ariopp:ariopp@cluster0.fpdbynt.mongodb.net/?retryWrites=true&w=majority")
+MONGO_DB_URI = "mongodb+srv://ariopp:ariopp@cluster0.fpdbynt.mongodb.net/?retryWrites=true&w=majority"
